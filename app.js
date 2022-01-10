c6b861be6d0e871d90967497feab2ab451e39b58
Johnchizzy <johnchizzy85@gmail.com>
2022-01-11 00:40:52 +0100

    my first commit

---
+++ app.js
@@ -0,0 +1,71 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+
+
+const app = express();
+let posts = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/blogDB", {useUnifiedTopology:true, useNewUrlParser: true });
+
+const postsSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Post = mongoose.model("Post", postsSchema);
+
+
+
+app.get("/", function(req, res){
+
+  Post.find({}, function(err, posts){
+    res.render("home", {posts: posts});
+  });
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+})
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+})
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+  post.save(function(err){
+    if (!err) {
+      res.redirect("/");
+    }
+  });
+
+});
+
+app.get("/posts/:postId", function(req, res){
+  const requestedPostId = req.params.postId;
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    if (err) {
+      console.log(err)
+    }
+    else {
+      res.render("post", {title: post.title, content: post.content});
+    }
+  });
+
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
